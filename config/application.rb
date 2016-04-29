8b32db2aced1bc3a9c2971225df466a30bc4e922
Igor C. Batista <igor.batista@gmail.com>
2016-04-29 16:41:09 -0300

    config assets precompile

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Controlfinan
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.js)
   end
 end
