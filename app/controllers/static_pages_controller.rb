6078564de0b43211cd1f0caead94a1123db79851
Igor C. Batista <igor.batista@gmail.com>
2017-02-07 10:16:01 -0300

    total credits dashboard

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,12 +4,13 @@ class StaticPagesController < ApplicationController
   def dashboard
   	accounts = CurrentAccount.month_current(date_br(DateTime.now.to_date)).select([:date_ocurrence]).group(:date_ocurrence).sum('price*type_launche')
   	credits = CurrentAccount.month_credit(date_br(DateTime.now.to_date)).select([:date_ocurrence]).group(:date_ocurrence).sum('price*type_launche')
-    #@current_accounts = accounts.sort! { |x, y| x[0] <=> y[0] }
-    #@current_account_credits = credits.sort! { |x, y| x[0] <=> y[0] }
+  	@total_accounts = CurrentAccount.month_current(date_br(DateTime.now.to_date)).sum('price*type_launche')
+  	@total_credits  = CurrentAccount.month_credit(date_br(DateTime.now.to_date)).sum('price*type_launche')
     
-    # total de creditos e debitos
+    # total de creditos
     @current_accounts = accounts.sort { |x, y| y[0] <=> x[0] }
     @current_account_credits = credits.sort { |x, y| y[0] <=> x[0] }
+
 
   end
 end
